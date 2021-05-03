aa816de363edafdcf7271b839b3b85d3325de911
emadunan <53171704+emadunan@users.noreply.github.com>
2021-05-03 16:22:11 +0200

    Delete __pycache__ and unneede Javascript code

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Delivery
 
 ### About the application
+
+Folder to untrack
+__pycache__
--- delivery/static/delivery/delivery.js
+++ delivery/static/delivery/delivery.js
@@ -1,11 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
 
-    const dropdown = document.querySelector('.dropdown-toggle');
-
-    dropdown.addEventListener('click', function () {
-        console.log(this);
-    })
-
-})
 
 
