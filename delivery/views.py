9a345931ca4a675e6e7cf862e1cf27dc1044e24e
emadunan <emadunan@gmail.com>
2021-05-03 21:15:29 +0200

    register, login views created

--- delivery/views.py
+++ delivery/views.py
@@ -1,6 +1,67 @@
 from django.shortcuts import render
 
+from .models import User, Item, Order
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 
 def index(request):
     return render(request, "delivery/index.html")
+
+
+def register(request):
+    if request.method == "POST":
+
+        # Get registeration values
+        username = request.POST["username"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        address = request.POST["address"]
+        phone = request.POST["phone"]
+
+        # Ensure password matches confirmation
+        if password != confirmation:
+            return render(request, "delivery/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Create new user
+        try:
+            user = User.objects.create_user(username, email=None, password=password, address=address, phone=phone)
+            user.save()
+        except IntegrityError:
+            return render(request, "delivery/register.html", {
+                "message": "Username is already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+
+    else:
+        return render(request, "delivery/register.html")
+
+
+def login_user(request):
+    if request.method == "POST":
+
+        # Login the user
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "delivery/login.html", {
+                "message": "Invalid username or password."
+            })
+    else:
+        return render(request, "delivery/login.html")
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
